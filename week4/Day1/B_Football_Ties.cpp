3c244c1c256049785a1d939f9dc9f8b412de6ebc
Md Ikram <86195681+kaziikram678@users.noreply.github.com>
2024-11-17 23:26:23 +0600

    Add files via upload
    Week4 Day1 Code Added

---
+++ week4/Day1/B_Football_Ties.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int x = n%3;
+        int y = m%3;
+
+        cout<<x<<endl;
+    }
+    return 0;
+}

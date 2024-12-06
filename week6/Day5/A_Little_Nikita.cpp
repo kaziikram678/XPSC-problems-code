e11d8a5c542dbf256c7fc0bc37f00e5a267839ba
Md Ikram <86195681+kaziikram678@users.noreply.github.com>
2024-12-06 22:14:55 +0600

    Add files via upload
    Week6 Day5 Code Added

---
+++ week6/Day5/A_Little_Nikita.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, x, y;
+        cin >> x >> y;
+        int f = 0;
+
+        if (y <= x && y % 2 == x % 2)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
